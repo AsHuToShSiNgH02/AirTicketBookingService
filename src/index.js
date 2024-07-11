4ab954b7e0d1822c221cc744cf0be75a7dfd2790
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2024-07-11 16:40:24 +0530

    added rabbit mq config

--- src/index.js
+++ src/index.js
@@ -7,18 +7,15 @@ const apiRoutes = require('./routes/index');
 const db = require('./models/index');
 
 const setupAndStartServer = () => {
-
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-
     app.use('/api', apiRoutes);
-
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
         if(process.env.DB_SYNC) {
             db.sequelize.sync({alter: true});
         }
-        console.log(FLIGHT_SERVICE_PATH)
+        // console.log(FLIGHT_SERVICE_PATH)
     });
 }
 
